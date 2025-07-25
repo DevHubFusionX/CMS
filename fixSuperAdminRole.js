e12e78019dfbf58cb651163442f756409127b773
Franklin Fanya <franklinfanya@gmail.com>
2025-07-25 22:04:41 +0100

    Add script to fix superadmin legacyRole and update database

---
+++ fixSuperAdminRole.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const User = require('./models/User');
+
+dotenv.config();
+
+const fixSuperAdminRole = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log('Connected to MongoDB');
+    
+    const result = await User.updateOne(
+      { email: 'superadmin@fusionx.com' },
+      { legacyRole: 'super_admin' }
+    );
+    
+    console.log('Update result:', result);
+    
+    const user = await User.findOne({ email: 'superadmin@fusionx.com' });
+    console.log('Updated user:', user);
+    
+    process.exit(0);
+  } catch (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+};
+
+fixSuperAdminRole();
